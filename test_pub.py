15b4256936e4f3990a0abf81bf58c8dbd6fa2c7b
Ahnaf <ahnaf.39@alumni.ubc.ca>
2021-05-04 16:51:52 -0700

    add test publisher

---
+++ test_pub.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    a = 1
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        a = -a
+        diclist = [
+            {
+            "ID": 0,
+            "Name": "Tim",
+            "Depth": 8,
+            "Angle": a,
+            },
+            {
+            "ID": 1,
+            "Name": "Roy",
+            "Depth": 8,
+            "Angle": -a,
+            },
+        ]
+        rospy.loginfo(diclist)
+        pub.publish(diclist)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass

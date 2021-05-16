d13d7f11961f67606ac26c6751cddda1372e9b52
Andrew <andrewxieax1@gmail.com>
2021-05-15 17:12:27 -0700

    Turtlebot edits to callback

--- Turtlebot.py
+++ Turtlebot.py
@@ -3,7 +3,9 @@ import tf
 import geometry_msgs
 import rospy
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 import math
+import json
 
 class TurtleBot():
 	DEPTH = 5
@@ -12,30 +14,30 @@ class TurtleBot():
 	def __init__(self):
 		# initiliaze
 		self.error_depth = 0
-		self.id = 0
+  		self.error_angle = 0
+		self.id = 'person 1'
+  		# Create a publisher which can "talk" to TurtleBot and tell it to move
+		self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+  
 		self.error_depth_angle = 0
 		self._mode = "resting"
 		rospy.init_node('Turtlebot_wrapper', anonymous=False)
 
 	@property
-    def mode(self):
-        print("Turtlebot mode: " + self._mode)
-        return self._mode
+	def mode(self):
+		print("Turtlebot mode: " + self._mode)
+		return self._mode
 
-    @mode.setter
-    def mode(self, mode):
-        print("Turtlebot mode: " + self._mode)
-        if not mode in self.MODES:
+	@mode.setter
+	def mode(self, mode):
+		print("Turtlebot mode: " + self._mode)
+		if not mode in self.MODES:
 			raise ValueError("Invalid Turtlebot Mode")
-        self._mode = mode
+		self._mode = mode
 
 	def new_dir(self, x,y):
-		# Create a publisher which can "talk" to TurtleBot and tell it to move
-		self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-
 		# Twist is a datatype for velocity
 		move_cmd = Twist()
-
 		move_cmd.linear.x = x
 		move_cmd.angular.z = y
 		self.cmd_vel.publish(move_cmd)
@@ -43,9 +45,14 @@ class TurtleBot():
 	def stop(self):
 		self.new_dir(0,0)
 	
-	def follow(self, plist):
+	def follow(self, data):
 		if self.mode != 'follow':
 			return
+		plist = json.loads(data.data)
+		if not plist:
+			self.stop
+   			return
+  		print(plist)
 		# list of a person being followed - ID, Name, Depth, Right/Left
 		# Complete right is 1 and complete left is -1
 		follow_id = self.id
@@ -53,17 +60,16 @@ class TurtleBot():
 		if res:
 			error_depth_prev = self.error_depth
 			error_angle_prev = self.error_angle
-			error_depth = res['depth'] - DEPTH
+			error_depth = res['depth'] - self.DEPTH
 			error_angle = res['angle'] - 0
 			p = 0.2 * error_depth
-			p_angle = 0.2 * error_angle
+			p_angle = 0.3 * error_angle
 			linearX = p
-			if res['angle'] > 0:
-				AngularY = p_angle
-			elif res['angle'] < 0:
+			if res['angle'] != 0:
 				AngularY = -p_angle
 			else:
 				AngularY = 0
+			print(AngularY)
 			self.new_dir(linearX,AngularY)
 			self.error_depth = error_depth
 			self.error_angle = error_angle
@@ -94,27 +100,32 @@ class TurtleBot():
 		# stop turtlebot
 			rospy.loginfo("Stopped TurtleBot")
 		# a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
-			self.cmd_vel.publish(Twist())
+			self.stop()
 		# sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
 			rospy.sleep(1)
 
-def callback(data):
-	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-	loaded_dictionary = json.loads(data.data)
-	if loaded_dictionary['state'] == "follow":
-		tbot.follow(loaded_dictionary['details'])
+# def callback(data):
+# 	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+# 	if loaded_dictionary['state'] == "follow":
+# 		tbot.follow(loaded_dictionary['details'])
 	# getattr(tbot, loaded_dictionary['state'])()
 
 def set_mode(data):
 	"""data: str"""
+	rospy.loginfo(rospy.get_caller_id() + " I heard %s", data.data)
 	tbot.mode = data.data
+	print(tbot.mode)
 
 if __name__ == '__main__':
-	try:
+ 	try:
 		tbot = TurtleBot()
+		print(rospy.is_shutdown())
 		# Function on ctrl+c
 		rospy.on_shutdown(tbot.shutdown)
+
 		rospy.Subscriber("tbot/state", String, set_mode)
+		# TODO: use other  message types other than JSON String for the following subscribers
+		rospy.Subscriber("ppl_detected", String, tbot.follow) 
 		# spin() simply keeps python from exiting until this node is stopped
 		rospy.spin()
 	except:
